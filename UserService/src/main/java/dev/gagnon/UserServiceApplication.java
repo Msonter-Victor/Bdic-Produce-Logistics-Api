3503247a790b0712866548868c0ad0e3f609e5a7
Bemgba <bemgbautor@gmail.com>
2025-04-11 15:08:15 +0100

    Development brach by Bemgba

---
+++ UserService/src/main/java/dev/gagnon/UserServiceApplication.java
@@ -0,0 +1,12 @@
+package dev.gagnon;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
